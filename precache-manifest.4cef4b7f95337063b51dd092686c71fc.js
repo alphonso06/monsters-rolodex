a9d58d39d83e24410c05d565d1a2939116e57461
Alphonso <alphonso@xaviermachina.com>
2020-02-03 13:12:27 +0800

    Updates

---
+++ precache-manifest.4cef4b7f95337063b51dd092686c71fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89655c0016ebd705d1e84f3cec06e09a",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9c4274f466b10963fb04",
+    "url": "/monsters-rolodex/static/css/main.506d8aae.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c4274f466b10963fb04",
+    "url": "/monsters-rolodex/static/js/main.ccab31cd.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
